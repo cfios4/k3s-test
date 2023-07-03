4f0ed8c6bcd6263ca115009afc9c39bb82cd7b3f
cfios4 <62921651+cfios4@users.noreply.github.com>
2023-07-03 19:30:28 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,79 @@ provider "proxmox" {
 resource "proxmox_vm_qemu" "fedora-vm" {
   count = 3
   target_node = "proxmox"
-  name = "k3s-"
+  name = "k3s-server"
+  clone = "k3s-template"
+  full_clone = false
+  memory = "512"
+  cpu = "host"
+  sockets = "1"
+  cores = "1"
+  
+  disk {
+    slot = 0
+    size = "4G"
+    storage = "local-lvm"
+	  type = "sata"
+  }
+  
+  network {
+    model = "virtio"
+    bridge = "vmbr0"
+  }
+  
+  connection {
+    type     = "ssh"
+    user     = "root"
+    password = "Admin!!1"
+    host     = "k3s-"
+  }
+  
+  provisioner "remote-exec" {
+    inline = [
+      "apk add -U python3",
+    ]
+  }
+
+resource "proxmox_vm_qemu" "fedora-vm" {
+  count = 3
+  target_node = "proxmox"
+  name = "k3s-agent1"
+  clone = "k3s-template"
+  full_clone = false
+  memory = "512"
+  cpu = "host"
+  sockets = "1"
+  cores = "1"
+  
+  disk {
+    slot = 0
+    size = "4G"
+    storage = "local-lvm"
+	  type = "sata"
+  }
+  
+  network {
+    model = "virtio"
+    bridge = "vmbr0"
+  }
+  
+  connection {
+    type     = "ssh"
+    user     = "root"
+    password = "Admin!!1"
+    host     = "k3s-"
+  }
+  
+  provisioner "remote-exec" {
+    inline = [
+      "apk add -U python3",
+    ]
+  }
+
+resource "proxmox_vm_qemu" "fedora-vm" {
+  count = 3
+  target_node = "proxmox"
+  name = "k3s-agent2"
   clone = "k3s-template"
   full_clone = false
   memory = "512"
